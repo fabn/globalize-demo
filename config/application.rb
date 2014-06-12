7710d41fa21c935149cc371cd548b9c53e21145e
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-06-12 20:26:42 +0200

    Installed active admin without namespace

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,10 @@ module GlobalizeDemo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:de, :en, :hu, :it, :'pt-BR', :'pt-PT']
+    # @see http://stackoverflow.com/questions/20361428/rails-i18n-validation-deprecation-warning
+    I18n.config.enforce_available_locales = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"

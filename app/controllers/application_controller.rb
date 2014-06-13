13d14ebe2453fd1d8a1c31de407f83d9d170b35a
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-06-13 13:43:17 +0200

    Automatically login first user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :login_default_user
+
+  def login_default_user
+    sign_in AdminUser.first unless current_admin_user
+  end
 end

725ce8000dd933e07ae4453f31b8394ddd504cc1
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-06-13 14:16:59 +0200

    AdminUser in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create an user
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
